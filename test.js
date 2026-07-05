c6d7949baca7dabdb58eb1205d7017888dc636a4
Caffeine-rohit <sharmarohit11506@gmail.com>
2026-07-06 02:46:28 +0530

    Remove test file

--- test.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3000/api/chat', { 
-  method: 'POST', 
-  headers: { 'Content-Type': 'application/json' }, 
-  body: JSON.stringify({ messages: [{ role: 'user', content: 'hello' }] }) 
-}).then(res => res.text()).then(console.log).catch(console.error);
